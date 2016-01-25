8cf385d0b0fa4e2c58edecd393ec49b79202b330
WilliamRutherford <william@williamrutherford.org>
2016-01-25 12:47:47 -0500

    SquareCircle updated
    began writing code

--- SquareCircle.cpp
+++ SquareCircle.cpp
@@ -1,6 +1,18 @@
 //SquareCircle.cpp
 
+bool inCircle(int radius, int i, int j)
+{
+	return 
+}
 
+int areaCircle(int radius)
+{
+	for(int i = 0; i < radius; i++)
+	{
+
+	}
+	return area;
+}
 
 int main()
 {
