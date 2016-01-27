51683624702ca2eed7ea9414cd416cdf32173e28
WilliamRutherford <william@williamrutherford.org>
2016-01-26 22:05:35 -0500

    areaCircle
    make the function actually work

--- SquareCircle.cpp
+++ SquareCircle.cpp
@@ -7,10 +7,20 @@ bool inCircle(int radius, int i, int j)
 
 int areaCircle(int radius)
 {
-	for(int i = 0; i < radius; i++)
+	//All methods should keep the origin in the top left, in the bottom right quadrant
+	unsigned long long int counter = 0 
+	//Method 1: original method.
+	
+	for(int j = 1; j <= radius; i++)
 	{
-
+		for(int i = radius; i > 0; i--)
+		{
+			if(inCircle(radius, i, j)):
+				area += i;
+				j++;
+		}
 	}
+	unsigned long long int area = (4*radius) + (4*counter) + 1;
 	return area;
 }
 
