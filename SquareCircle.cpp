f29c6185df56b2c7e9b04b88277d7a8a5ea5d8dd
William Rutherford <william@williamrutherford.org>
2016-01-29 20:31:34 -0500

    Update SquareCircle.cpp
    Now in 64-bit unsigned integers, Radius is a global variable

--- SquareCircle.cpp
+++ SquareCircle.cpp
@@ -1,26 +1,31 @@
 //SquareCircle.cpp
+#include <math.h>
+#include <iostream>
+using namespace std;
 
-bool inCircle(int radius, int i, int j)
+unsigned long long int RADIUS = 1000;
+
+bool inCircle(int i, int j)
 {
-	return pow(i, 2) + pow(j, 2) <= pow(radius, 2)
+	return pow(i, 2) + pow(j, 2) <= pow(RADIUS, 2) //Pythagorean Theorem
 }
 
-int areaCircle(int radius)
+int areaCircle()
 {
 	//All methods should keep the origin in the top left, in the bottom right quadrant
 	unsigned long long int counter = 0 
 	//Method 1: original method.
 	
-	for(int j = 1; j <= radius; i++)
+	for(int j = 1; j <= RADIUS; i++)
 	{
-		for(int i = radius; i > 0; i--)
+		for(int i = RADIUS; i > 0; i--)
 		{
-			if(inCircle(radius, i, j)):
+			if(inCircle(i, j)):
 				area += i;
 				j++;
 		}
 	}
-	unsigned long long int area = (4*radius) + (4*counter) + 1;
+	unsigned long long int area = (4*RADIUS) + (4*counter) + 1;
 	return area;
 }
 
